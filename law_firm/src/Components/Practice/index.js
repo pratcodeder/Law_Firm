8cfe1a1b1139e08849d3722fcd8c3733595263be
Prathmesh Gawade <gawadepr101@gmail.com>
2023-11-30 16:28:16 +0530

    Area of practices completed

--- law_firm/src/Components/Practice/index.js
+++ law_firm/src/Components/Practice/index.js
@@ -1,7 +1,15 @@
 import "./Practice.css"
 
 export default () => {
-    let image = ["business1", "partner", "realEas", "business2", "landlord", "elder"]
+    let image = [
+        {"IMG": "business1", "title":"BUSINESS LAW"}, 
+        {"IMG": "partner", "title":"Partnership LAW"},
+        {"IMG": "realEas", "title":"REAL ESTATE LAW"},
+        {"IMG": "business2", "title":"BUSINESS LAW"},
+        {"IMG": "landlord", "title":"LANDLORD DISPUTES"},
+        {"IMG": "elder", "title":"ELDER ABUSE"}
+        
+        ]
     return (
         <div className="PracticeSection">
             <div className="heading"><h1>Area of Practices</h1></div>
@@ -9,12 +17,22 @@ export default () => {
                { image.map((img, index) => {
                 if(index==0 || index==3 || index==4) {
                 return (
-                    <div className={`image img${index} `}><img src={`./${img}.png`} /></div>
+                   
+                    <div className={`gridImage img${index} `}>
+                        <img src={`./${img.IMG}.png`} />
+                    <span className="title">{img.title}</span>
+                        </div>
+                  
                 )
                 }
                 return (
-                    <div className={`image1 img${index} `}><img src={`./${img}.png`} /></div>
-                )
+                  
+                    <div className={`gridImage1 img${index} `}>
+                        <img src={`./${img.IMG}.png`} />
+                    <span className="title">{img.title}</span>
+                        </div>
+                    
+                    )
 
 
                })} 
